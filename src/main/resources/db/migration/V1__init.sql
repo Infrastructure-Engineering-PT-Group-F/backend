d01614dd61ec66f4b8752ececda5acd6954ccf49
R41f-K <ralf.kuehmayer@gmail.com>
2026-06-18 17:44:47 +0200

    feat: #8 switch persistence to PostgreSQL via env-selected profile

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -13,13 +13,13 @@ CREATE TABLE FAVORITE_LOCATIONS (
 	user_id varchar(255) not null,
 	given_name varchar(255) not null,
 	given_location varchar(255) not null,
-	longitude double not null,
-	latitude double not null,
-	elevation float not null,
+	longitude double precision not null,
+	latitude double precision not null,
+	elevation real not null,
 	nearest_airport varchar(255) not null,
-	nearest_airport_longitude double not null,
-	nearest_airport_latitude double not null,
-	nearest_airport_elevation float not null
+	nearest_airport_longitude double precision not null,
+	nearest_airport_latitude double precision not null,
+	nearest_airport_elevation real not null
 );
 
 INSERT INTO FAVORITE_LOCATIONS (id, user_id, given_name, given_location, longitude, latitude, elevation, nearest_airport, nearest_airport_longitude, nearest_airport_latitude, nearest_airport_elevation) VALUES ('c5b38625-7eed-4705-858d-c685f18ed47d', '81150016-8501-4b97-9168-01113e21d8a5', 'Linz', 'Vienna, Austria', 16.37208, 48.20849, 171.0, 'LOWW', 16.5697, 48.110298, 183.0);
